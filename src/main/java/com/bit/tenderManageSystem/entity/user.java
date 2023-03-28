b830d41365e6b97d9d601c919ccec52d64b32c07
xianjun <xianjteng@163.com>
2023-03-28 17:14:28 +0800

    项目构建

---
+++ src/main/java/com/bit/tenderManageSystem/entity/user.java
@@ -0,0 +1,97 @@
+package com.bit.tenderManageSystem.entity;
+
+import java.util.Date;
+
+public class user {
+
+    private int userId;
+    private String userName;
+    private String password;
+    private String phone;
+    private String ticket;
+    private String salt;
+    private String email;
+    private int type;
+    private Date lastLoginTime;
+    private int status;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
---
+++ src/main/java/com/bit/tenderManageSystem/util/RandomUtil.java
@@ -0,0 +1,7 @@
+package com.bit.tenderManageSystem.util;
+
+/**
+ * 随机生成验证码
+ */
+public class RandomUtil {
+}
